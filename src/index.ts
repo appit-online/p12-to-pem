7b1daa3d04cfba5a196506be125509c7b915ec1d
David Koller <david.koller@mhp.com>
2020-04-05 17:36:01 +0200

    -update version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import {convertToPem} from './lib/p12'
+import { convertToPem } from './lib/p12';
 import * as fs from 'fs';
 
 export const Name = (name: string) => `Hello ${name}`;
 
-export function getPemFromP12(certPath: string, password: string){
-    const p12File = fs.readFileSync(certPath, {encoding: 'binary'});
-    return convertToPem(p12File, password);
+export function getPemFromP12(certPath: string, password: string) {
+  const p12File = fs.readFileSync(certPath, { encoding: 'binary' });
+  return convertToPem(p12File, password);
 }
